a84373d27c1017d3935afab8d490465598af6eaf
adarsh singh chauhan <9017456+iamadarsh47@users.noreply.github.com>
2019-07-10 23:05:21 +0530

    Create CollegeReg.aspx.vb

---
+++ CollegeReg.aspx.vb
@@ -0,0 +1,99 @@
+Imports System.data.sqlclient
+Public Class CollegeReg
+    Inherits System.Web.UI.Page
+    Dim con As SqlConnection
+    Protected WithEvents TextBox6 As System.Web.UI.WebControls.TextBox
+    Protected WithEvents TextBox7 As System.Web.UI.WebControls.TextBox
+    Protected WithEvents Label6 As System.Web.UI.WebControls.Label
+    Protected WithEvents Label7 As System.Web.UI.WebControls.Label
+    Protected WithEvents RequiredFieldValidator6 As System.Web.UI.WebControls.RequiredFieldValidator
+    Protected WithEvents RequiredFieldValidator7 As System.Web.UI.WebControls.RequiredFieldValidator
+    Protected WithEvents CompareValidator1 As System.Web.UI.WebControls.CompareValidator
+    Protected WithEvents RegularExpressionValidator2 As System.Web.UI.WebControls.RegularExpressionValidator
+    Dim cmd As SqlCommand
+
+#Region " Web Form Designer Generated Code "
+
+    'This call is required by the Web Form Designer.
+    <System.Diagnostics.DebuggerStepThrough()> Private Sub InitializeComponent()
+
+    End Sub
+    Protected WithEvents Label1 As System.Web.UI.WebControls.Label
+    Protected WithEvents Label2 As System.Web.UI.WebControls.Label
+    Protected WithEvents Label3 As System.Web.UI.WebControls.Label
+    Protected WithEvents Label4 As System.Web.UI.WebControls.Label
+    Protected WithEvents Label5 As System.Web.UI.WebControls.Label
+    Protected WithEvents TextBox1 As System.Web.UI.WebControls.TextBox
+    Protected WithEvents TextBox2 As System.Web.UI.WebControls.TextBox
+    Protected WithEvents TextBox3 As System.Web.UI.WebControls.TextBox
+    Protected WithEvents TextBox4 As System.Web.UI.WebControls.TextBox
+    Protected WithEvents TextBox5 As System.Web.UI.WebControls.TextBox
+    Protected WithEvents RequiredFieldValidator1 As System.Web.UI.WebControls.RequiredFieldValidator
+    Protected WithEvents RequiredFieldValidator2 As System.Web.UI.WebControls.RequiredFieldValidator
+    Protected WithEvents RequiredFieldValidator3 As System.Web.UI.WebControls.RequiredFieldValidator
+    Protected WithEvents RequiredFieldValidator4 As System.Web.UI.WebControls.RequiredFieldValidator
+    Protected WithEvents RequiredFieldValidator5 As System.Web.UI.WebControls.RequiredFieldValidator
+    Protected WithEvents RegularExpressionValidator1 As System.Web.UI.WebControls.RegularExpressionValidator
+    Protected WithEvents Button1 As System.Web.UI.WebControls.Button
+
+    'NOTE: The following placeholder declaration is required by the Web Form Designer.
+    'Do not delete or move it.
+    Private designerPlaceholderDeclaration As System.Object
+
+    Private Sub Page_Init(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Init
+        'CODEGEN: This method call is required by the Web Form Designer
+        'Do not modify it using the code editor.
+        InitializeComponent()
+    End Sub
+
+#End Region
+
+    Private Sub Page_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        'Put user code to initialize the page here
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        con = New SqlConnection
+        con.ConnectionString = "server=vb; database=adarshvb; uid=14bcs003; pwd=7298917198"
+        con.Open()
+        cmd = New SqlCommand
+        cmd.Connection = con
+        cmd.CommandText = "INSERT INTO ACollegeReg values ('" & TextBox1.Text & "', '" & TextBox2.Text & "', '" & TextBox3.Text & "', '" & TextBox4.Text & "', '" & TextBox5.Text & "', '" & TextBox6.Text & "')"
+        Try
+            cmd.ExecuteNonQuery()
+            Response.Redirect("~/CollegeLogin.aspx")
+        Catch ex As Exception
+            Response.Write("registration not successful")
+        End Try
+    End Sub
+
+
+
+    Private Sub TextBox1_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles TextBox1.TextChanged
+
+    End Sub
+
+    Private Sub TextBox2_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles TextBox2.TextChanged
+
+    End Sub
+
+    Private Sub TextBox3_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles TextBox3.TextChanged
+
+    End Sub
+
+    Private Sub TextBox4_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles TextBox4.TextChanged
+
+    End Sub
+
+    Private Sub TextBox5_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles TextBox5.TextChanged
+
+    End Sub
+
+    Private Sub TextBox6_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles TextBox6.TextChanged
+
+    End Sub
+
+    Private Sub TextBox7_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles TextBox7.TextChanged
+
+    End Sub
+End Class
